35f1acf1ad8c3e42fa0415614370ba8d34b5cd6b
Joedang <shields6@pdx.edu>
2020-12-18 06:25:04 -0800

    more XF86 keys; concise-er cases; appearance tweak

--- bindgraph.sh
+++ bindgraph.sh
@@ -8,6 +8,7 @@ output=bindings.html # file to be [over]written
 # Edit this to match the *keysyms* of your keyboard+layout. Use "EOL" to break rows. (Use `xev` to figure out your keysyms.)
 declare -a keyLayout=( \
 XF86AudioMute XF86AudioLowerVolume XF86AudioRaiseVolume XF86AudioMicMute XF86Launch1 EOL \
+XF86WLAN XF86WebCam XF86Display XF86MonBrightnessDown XF86MonBrightnessUp XF86AudioPrev XF86AudioPlay XF86AudioNext EOL \
 Escape F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12 Home End Insert Delete EOL \
 grave 1 2 3 4 5 6 7 8 9 0 minus equal BackSpace EOL \
 Tab q w e r t y u i o p bracketleft bracketright bar EOL \
@@ -21,41 +22,54 @@ button1 button2 button3 \
 translateNames() {
     # Translate the keysym into what will be displayed. Edit to match what's physically on your keyboard.
     case "$key" in 
+        Escape | XF86* | F[0-9]* | Home | End | Insert | Delete | Prior | Next | Up | Down | Left | Right)
+            adhocStyle+=' font-size: var(--tiny-key-height); min-width: var(--tiny-key-width);' 
+            ;;
+    esac
+    case "$key" in 
         XF86AudioMute)        keyName='🔇' ;;
         XF86AudioLowerVolume) keyName='🔉' ;;
         XF86AudioRaiseVolume) keyName='🔊' ;;
         XF86AudioMicMute)     keyName='❌🎤' ;;
         XF86Launch1)          keyName='🚀' ;;
-        Escape)               adhocStyle='min-width: calc(1.3 * var(--key-width)); font-size: var(--tiny-key-height);'; keyName='Esc' ;;
-        F[0-9]* | Home | End) adhocStyle=' font-size: var(--tiny-key-height); min-width: var(--tiny-key-width);' ;;
-        Insert)       adhocStyle=' font-size: var(--tiny-key-height); min-width: var(--tiny-key-width);'; keyName='Ins' ;;
-        Delete)       adhocStyle='min-width: calc(1.3 * var(--key-width)); font-size: var(--tiny-key-height);'; keyName='Del' ;;
+        XF86WLAN)             keyName='📡' ;;
+        XF86WebCam)           keyName='📸🎤' ;; 
+        XF86Display)          keyName='📽' ;;
+        XF86MonBrightnessDown)keyName='🌞-' ;;
+        XF86MonBrightnessUp)  keyName='🌞+' ;;
+        XF86AudioPrev)        keyName='|◂' ;;
+        XF86AudioPlay)        keyName='▸⏸' ;;
+        XF86AudioNext)        keyName='▸|' ;;
+        Escape)               adhocStyle+='min-width: calc(1.3 * var(--key-width));'; keyName='Esc' ;;
+        F[0-9]* | Home | End) ;;
+        Insert)       keyName='Ins' ;;
+        Delete)       adhocStyle+='min-width: calc(1.3 * var(--key-width));'; keyName='Del' ;;
         grave)        keyName='~' ;;
         minus)        keyName='-' ;;
         equal)        keyName='=' ;;
-        BackSpace)    adhocStyle='min-width: calc(2.2 * var(--key-width));'; keyName='Backsp' ;;
-        Tab)          adhocStyle='min-width: calc(1.5 * var(--key-width));' ;;
+        BackSpace)    adhocStyle+='min-width: calc(2.2 * var(--key-width));'; keyName='Backsp' ;;
+        Tab)          adhocStyle+='min-width: calc(1.5 * var(--key-width));' ;;
         bracketleft)  keyName='[' ;;
         bracketright) keyName=']' ;;
-        bar)          adhocStyle='min-width: calc(1.5 * var(--key-width));'; keyName='|' ;;
+        bar)          adhocStyle+='min-width: calc(1.5 * var(--key-width));'; keyName='|' ;;
         #CapsLock
         semicolon)    keyName=';' ;;
         apostrophe)   keyName="'" ;;
-        Return)       adhocStyle='min-width: calc(2.5 * var(--key-width));' ;;
-        Shift)        adhocStyle='min-width: calc(2.5 * var(--key-width));' ;;
+        Return)       adhocStyle+='min-width: calc(2.5 * var(--key-width));' ;;
+        Shift)        adhocStyle+='min-width: calc(2.5 * var(--key-width));' ;;
         comma)        keyName=',' ;;
         period)       keyName='.' ;;
         slash)        keyName='/' ;;
-        Ctrl)         adhocStyle='min-width: calc(1.3 * var(--key-width));' ;;
-        Mod4)         keyName='Sup' ;;
+        Ctrl)         adhocStyle+='min-width: calc(1.3 * var(--key-width));' ;;
+        Mod4)         keyName='🪟' ;;
         Mod1)         keyName='Alt' ;;
-        space)        adhocStyle='min-width: calc(6   * var(--key-width));' ;;
-        Prior)        adhocStyle='font-size: var(--tiny-key-height); min-width: var(--tiny-key-width);'; keyName='PgUp' ;;
-        Next)         adhocStyle='font-size: var(--tiny-key-height); min-width: var(--tiny-key-width);'; keyName='PgDn' ;;
-        Up)           adhocStyle='font-size: var(--tiny-key-height); min-width: var(--tiny-key-width);'; keyName='↑' ;;
-        Down)         adhocStyle='font-size: var(--tiny-key-height); min-width: var(--tiny-key-width);'; keyName='↓' ;;
-        Left)         adhocStyle='font-size: var(--tiny-key-height); min-width: var(--tiny-key-width);'; keyName='←' ;;
-        Right)        adhocStyle='font-size: var(--tiny-key-height); min-width: var(--tiny-key-width);'; keyName='→' ;;
+        space)        adhocStyle+='min-width: calc(6   * var(--key-width));' ;;
+        Prior)        keyName='PgUp' ;;
+        Next)         keyName='PgDn' ;;
+        Up)           keyName='↑' ;;
+        Down)         keyName='↓' ;;
+        Left)         keyName='←' ;;
+        Right)        keyName='→' ;;
         button1)      keyName='left click' ;;
         button2)      keyName='middle click' ;;
         button3)      keyName='right click' ;;
